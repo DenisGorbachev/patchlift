c61a4d0d27a4952bc19ad3ca88b5f7477d3645e6
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-02-22 21:01:43 +0700

    feat: prepare for publishing

--- lib/classes/Target.ts
+++ lib/classes/Target.ts
@@ -1,40 +1,40 @@
-import { AsDir } from "../interfaces/AsDir.ts"
+import type { AsDir } from "../interfaces/AsDir.ts"
 import { exists } from "jsr:@std/fs@0.224.0"
 
 import { copyAll, copyOne, isGitRepo, lefthookRunPreCommit, miseTrust } from "../functions.ts"
 
 export class Target implements AsDir {
-    private constructor(public dir: string) {}
+  private constructor(public dir: string) {}
 
-    static async create(dir: string | undefined) {
-        if (!dir) throw new Error("Target is required")
-        if (!await exists(dir)) throw new Error(`Target must exist: ${dir}`)
-        if (!(await Deno.stat(dir)).isDirectory) throw new Error(`Target must be a directory: ${dir}`)
-        if (!await isGitRepo(dir)) throw new Error(`Target must contain a git repository: ${dir}`)
-        return new Target(dir)
-    }
+  static async create(dir: string | undefined) {
+    if (!dir) throw new Error("Target is required")
+    if (!await exists(dir)) throw new Error(`Target must exist: ${dir}`)
+    if (!(await Deno.stat(dir)).isDirectory) throw new Error(`Target must be a directory: ${dir}`)
+    if (!await isGitRepo(dir)) throw new Error(`Target must contain a git repository: ${dir}`)
+    return new Target(dir)
+  }
 
-    toString() {
-        return this.dir
-    }
+  toString() {
+    return this.dir
+  }
 
-    asDir() {
-        return this.dir
-    }
+  asDir() {
+    return this.dir
+  }
 
-    miseTrust() {
-        return miseTrust(this.dir)
-    }
+  miseTrust() {
+    return miseTrust(this.dir)
+  }
 
-    lefthookRunPreCommit() {
-        return lefthookRunPreCommit(this.dir)
-    }
+  lefthookRunPreCommit() {
+    return lefthookRunPreCommit(this.dir)
+  }
 
-    copyOne(from: AsDir) {
-        return copyOne(from, this)
-    }
+  copyOne(from: AsDir) {
+    return copyOne(from, this)
+  }
 
-    copyAll(from: AsDir) {
-        return copyAll(from, this)
-    }
+  copyAll(from: AsDir) {
+    return copyAll(from, this)
+  }
 }
--- lib/functions.ts
+++ lib/functions.ts
@@ -1,26 +1,26 @@
 import { $ } from "npm:zx@8.3.2"
-import { AsDir } from "./interfaces/AsDir.ts"
+import type { AsDir } from "./interfaces/AsDir.ts"
 import { dirname, SEPARATOR } from "jsr:@std/path@0.224.0"
 
 export const isGitRepo = async (dir: string) => {
-    const output = await $({ cwd: dir })`git rev-parse --is-inside-work-tree`
-    return output.text().trim() === "true"
+  const output = await $({ cwd: dir })`git rev-parse --is-inside-work-tree`
+  return output.text().trim() === "true"
 }
 
 export const ensure = <T>(value: T | null | undefined, err: () => Error) => {
-    if (value === null || value === undefined) {
-        throw err()
-    } else {
-        return value
-    }
+  if (value === null || value === undefined) {
+    throw err()
+  } else {
+    return value
+  }
 }
 
 export const copyOne = (from: AsDir, to: AsDir) => async (path: string) => {
-    const source = from.asDir() + SEPARATOR + path
-    const target = to.asDir() + SEPARATOR + path
-    const targetParent = dirname(target)
-    await Deno.mkdir(targetParent, { recursive: true })
-    await Deno.copyFile(source, target)
+  const source = from.asDir() + SEPARATOR + path
+  const target = to.asDir() + SEPARATOR + path
+  const targetParent = dirname(target)
+  await Deno.mkdir(targetParent, { recursive: true })
+  await Deno.copyFile(source, target)
 }
 
 export const copyAll = (from: AsDir, to: AsDir) => (paths: string[]) => Promise.all(paths.map(copyOne(from, to)))
