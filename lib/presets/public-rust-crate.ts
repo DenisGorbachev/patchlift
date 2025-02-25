882e83f36b448ac6a68820e8c810661ed72fa365
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-02-25 13:26:51 +0700

    feat: applyToPublicRustCrateDir

--- lib/presets/public-rust-crate.ts
+++ lib/presets/public-rust-crate.ts
@@ -1,6 +1,13 @@
 import { applyPatches } from "../functions.ts"
 import { createUrlMacroLocalRepoSource } from "../classes/LocalRepoSource.ts"
 import { RUST_PUBLIC } from "../constants.ts"
+import { Target } from "../classes/Target.ts"
 
-const source = await createUrlMacroLocalRepoSource()
-export const applyToPublicRustCrate = applyPatches(source, RUST_PUBLIC)
+const urlMacroSource = await createUrlMacroLocalRepoSource()
+
+export const applyToPublicRustCrate = applyPatches(urlMacroSource, RUST_PUBLIC)
+
+export const applyToPublicRustCrateDir = async (targetDir: string) => {
+  const target = await Target.create(targetDir)
+  return applyPatches(urlMacroSource, RUST_PUBLIC)(target)
+}
