6d1786086d7492e598f38c629e0f8adf5733ef0c
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-02-27 13:03:33 +0700

    feat: add preset

---
+++ lib/presets/pre-public-rust-crate.ts
@@ -0,0 +1,13 @@
+import { applyPatches } from "../functions.ts"
+import { createUrlMacroLocalRepoSource } from "../classes/LocalRepoSource.ts"
+import { JUSTFILE, RUST_PRE_PUBLIC } from "../constants.ts"
+import { Target } from "../classes/Target.ts"
+
+const urlMacroSource = await createUrlMacroLocalRepoSource()
+
+export const applyPatchesToPrePublicRustCrate = applyPatches(urlMacroSource, RUST_PRE_PUBLIC, [...JUSTFILE])
+
+export const applyPatchesToPrePublicRustCrateDir = async (targetDir: string | undefined) => {
+  const target = await Target.create(targetDir)
+  return applyPatchesToPrePublicRustCrate(target)
+}
