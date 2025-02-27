314928cbb46d4a101448a0eb87c4d4a4b5845056
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-02-27 13:02:12 +0700

    fix: add preset

--- lib/presets/public-rust-crate.ts
+++ lib/presets/public-rust-crate.ts
@@ -1,13 +1,13 @@
 import { applyPatches } from "../functions.ts"
 import { createUrlMacroLocalRepoSource } from "../classes/LocalRepoSource.ts"
-import { RUST_PUBLIC } from "../constants.ts"
+import { JUSTFILE, RUST_PUBLIC } from "../constants.ts"
 import { Target } from "../classes/Target.ts"
 
 const urlMacroSource = await createUrlMacroLocalRepoSource()
 
-export const applyToPublicRustCrate = applyPatches(urlMacroSource, RUST_PUBLIC, [])
+export const applyToPublicRustCrate = applyPatches(urlMacroSource, RUST_PUBLIC, [...JUSTFILE])
 
 export const applyToPublicRustCrateDir = async (targetDir: string | undefined) => {
   const target = await Target.create(targetDir)
-  return applyPatches(urlMacroSource, RUST_PUBLIC, [])(target)
+  return applyToPublicRustCrate(target)
 }
